ba01a2d8affbd874d63ff765697b677ea14c8ac3
Kareem Saleh <kareemsal@gmail.com>
2014-02-04 20:05:23 -0500

    Added bootstrap NSFW switch.

--- public/js/client.js
+++ public/js/client.js
@@ -1,38 +1,53 @@
+var handleFailure = function() {
+
+}
+
+/*
+ * Handle when the AJAX is successful
+ */
+var handleSuccess = function(data, textStatus, jqXHR) {
+
+}
 
 /*
  * Submit the URL to the SFWMe API
  */
-var submitUrl = function(input_url) {
+var submitUrl = function(input_url, input_safe) {
 	input_url.addClass('loading');
 
-	$.post("save", { url: input_url.val(), source: "web" }, function(data, textStatus, jqXHR) {
-		console.log("Success:");
-		console.log(data);
-	}).fail(function() {
-		console.log("Fail:");
-		console.log(jqXHR);
-	}).always(function() {
+	$.post("save", { 
+		url: input_url.val(), 
+		safe: input_safe.val(), 
+		source: "web" }, 
+		handleSuccess)
+	.fail(handleFailure)
+	.always(function() {
 		input_url.removeClass('loading');
 	});
 };
 
 /*
- * jQery Ready function
+ * jQuery Ready function
  */
 $(document).ready(function() {
 	var input_url = $('#input-url');
 	var btn_go = $('#btn-go');
 	var div_options = $('#options');
+	var input_nsfw = $('#nsfw');
+
+	// Init Bootstrap-switch (stylized checkboxes)
+	input_nsfw.bootstrapSwitch();
+	input_nsfw.bootstrapSwitch('setOnLabel', 'NSFW');
+	input_nsfw.bootstrapSwitch('setOffLabel', 'SFW');
 
 	// Btn click & Htting the enter button.
 	btn_go.on('click', function() {
-		submitUrl(input_url);
+		submitUrl(input_url, input_nsfw);
 	});
 	input_url.keypress(function(e) {
 		if (e.which == 13) {
 			e.preventDefault();
-			$("form").submit();
-			submitUrl(input_url);
+			submitUrl(input_url, input_nsfw);
 		}
 	});
 
