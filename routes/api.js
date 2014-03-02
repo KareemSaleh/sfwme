9021e083f97336c5c4facbd49cc0faa624223930
Kareem Saleh <kareemsal@gmail.com>
2014-03-02 13:11:34 -0500

    Adding the beginnings of mongodb.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 var validator = require('validator'),
 	crypto = require('crypto'),
 	redis = require('redis'),
-	db = redis.createClient();
+	redisdb = redis.createClient(),
+	mongodb = require('mongodb').MongoClient,
+    format = require('util').format;
 
 const BASE_URL = process.env.BASE_URL;
  
@@ -35,9 +37,31 @@ var respondErr = function(res, msg) {
  * @param  {boolean} 	nsfw  true if this lnk is NSFW
  */
 var cacheIt = function(token, url, nsfw) {
-	db.hmset(url, {nsfw: nsfw, token:token}, redis.print);
-	db.hmset(token, {url: url}, redis.print);
+	redisdb.hmset(url, {nsfw: nsfw, token:token}, redis.print);
+	redisdb.hmset(token, {url: url}, redis.print);
 }
+
+var saveIt = function(token, url, nsfw) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+		if(err) throw err;
+
+		var collection = db.collection('test_insert');
+		collection.insert({a:2}, function(err, docs) {
+
+			collection.count(function(err, count) {
+				console.log(format("count = %s", count));
+			});
+
+			// Locate all the entries using find
+			collection.find().toArray(function(err, results) {
+				console.dir(results);
+				// Let's close the db
+				db.close();
+			});
+		});
+	});
+}
+
 
 // ---------------------------------------------------
 // Export Methods
@@ -66,7 +90,7 @@ exports.save = function(req, res) {
 	}
 
 	// Is this url already in our db?
-	db.hgetall(url, function(err, reply) {
+	redisdb.hgetall(url, function(err, reply) {
 
 		// reply is null when the key is missing
 		if (!reply) {
