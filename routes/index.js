7070fa7c01c72d9a725a311dcbda58feb1b5bbd8
Kareem Saleh <kareemsal@gmail.com>
2014-03-23 13:53:02 -0400

    Added popular link where I'll list top used links.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ exports.index = function(req, res){
 };
 
 exports.popular = function(req, res){
-	res.render('contact', { title: 'SFWMe' });
+	res.render('popular', { title: 'SFWMe' });
 }
 
 exports.about = function(req, res){
