b249dfd299a75a53198e20bb85823f630f7b76e3
Kareem Saleh <kareemsal@gmail.com>
2014-04-03 22:06:12 -0400

    Logging to a location with forever.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Update our dependencies
+npm update
+
 # Set ENV variables
 export BASE_URL='sfwme.com'
 export PORT=80
@@ -7,8 +10,5 @@ export MONGO_NODE_DRIVER_HOST=$BASE_URL
 export MONGO_NODE_DRIVER_PORT=27017
 export NODE_ENV='production'
 
-# Update our dependencies
-npm update
-
 # Start the Server using forever
-forever -a start -l forever.log -o sfwme.log -e err.log app.js
+forever -a start -l logs/forever.log -o logs/sfwme.log -e logs/err.log app.js
