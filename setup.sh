24200f44ab0ca30d7136aedd5cb45d58e5a235cb
Kareem Saleh <kareemsal@gmail.com>
2014-04-05 13:07:56 -0400

    Trying base path variable for forever script.

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ export PORT=80
 export MONGO_NODE_DRIVER_HOST=$BASE_URL
 export MONGO_NODE_DRIVER_PORT=27017
 export NODE_ENV='production'
+export BASE_PATH=~/sfwme
 
 # Start the Server using forever
-forever -a start -l ./logs/forever.log -o ./logs/sfwme.log -e ./logs/err.log app.js
+forever -a start -l $BASE_PATH/logs/forever.log -o $BASE_PATH/logs/sfwme.log -e $BASE_PATH/logs/err.log app.js
